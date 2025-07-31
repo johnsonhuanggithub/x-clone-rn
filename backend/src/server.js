87c0708333d102d2aabb628cd19023a286e5f136
huangj <huangj@spsd.sk.ca>
2025-07-31 15:15:18 -0600

    db models created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { createServer } from 'http';    
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";  
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+connectDB();
+
+app.get('/', (req, res) => {  res.send('Hello World!');});
+
+app.listen(ENV.PORT, () => {
+  console.log('Server is running on port :', ENV.PORT);
 });
