d031954ee1027ac3e132c869893491d847092829
huangj <huangj@spsd.sk.ca>
2025-07-31 15:28:36 -0600

    startServer function

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,19 @@ connectDB();
 
 app.get('/', (req, res) => {  res.send('Hello World!');});
 
-app.listen(ENV.PORT, () => {
-  console.log('Server is running on port :', ENV.PORT);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
+export default app;
